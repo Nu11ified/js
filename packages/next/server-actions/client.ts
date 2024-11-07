9a5ec31774922d66fbc421244fe92ccf7deebc69
Nu11ified <86795298+Nu11ified@users.noreply.github.com>
2024-11-07 15:59:21 -0600

    Update client.ts

--- packages/next/server-actions/client.ts
+++ packages/next/server-actions/client.ts
@@ -1,131 +1,90 @@
-'use server';
-
-import {
-  CookieStorage,
-  type SignInOptions,
-  type GetContextParameters,
-  type InteractionMode,
-} from '@logto/node';
+import { CookieStorage } from '@logto/node';
 import NodeClient from '@logto/node/edge';
+import LogtoNextBaseClient from '../src/client.js';
 
-import BaseClient from '../src/client';
-import type { LogtoNextConfig } from '../src/types.js';
-
-export type { LogtoContext, InteractionMode } from '@logto/node';
-
-export default class LogtoClient extends BaseClient {
-  constructor(config: LogtoNextConfig) {
-    super(config, {
-      NodeClient,
-    });
-  }
-
-  /**
-   * Start the sign-in flow with the specified options.
-   *
-   * @param options The options for the sign-in flow.
-   */
-  async handleSignIn(options: SignInOptions): Promise<{ url: string; newCookie?: string }>;
-  /**
-   * Init sign-in and return the url to redirect to Logto.
-   *
-   * @deprecated Use the object parameter instead.
-   * @param redirectUri the uri (callbackUri) to redirect to after sign in
-   * @param interactionMode OIDC interaction mode
-   * @returns the url to redirect
-   */
-  async handleSignIn(
-    redirectUri: string,
-    interactionMode?: InteractionMode
-  ): Promise<{ url: string; newCookie?: string }>;
-  async handleSignIn(
-    options: SignInOptions | string | URL,
-    interactionMode?: InteractionMode
-  ): Promise<{ url: string; newCookie?: string }> {
-    const nodeClient = await this.createNodeClient();
-    const finalOptions: SignInOptions =
-      typeof options === 'string' || options instanceof URL
-        ? { redirectUri: options, interactionMode }
-        : options;
-    await nodeClient.signIn(finalOptions);
-
-    if (!this.navigateUrl) {
-      // Not expected to happen
-      throw new Error('navigateUrl is not set');
+class LogtoClient extends LogtoNextBaseClient {
+    constructor(config) {
+        super(config, {
+            NodeClient,
+        });
     }
-
-    return {
-      url: this.navigateUrl,
-    };
-  }
-
-  /**
-   * Init sign-out and return the url to redirect to Logto.
-   *
-   * @param redirectUri the uri (postSignOutUri) to redirect to after sign out
-   * @returns the url to redirect to
-   */
-  async handleSignOut(redirectUri = this.config.baseUrl): Promise<string> {
-    const nodeClient = await this.createNodeClient();
-    await nodeClient.signOut(redirectUri);
-    await this.storage?.destroy();
-
-    if (!this.navigateUrl) {
-      // Not expected to happen
-      throw new Error('navigateUrl is not set');
-    }
-
-    return this.navigateUrl;
-  }
-
-  /**
-   * Handle sign-in callback from Logto.
-   *
-   * @param callbackUrl the uri (callbackUri) to redirect to after sign in, should match the one used in handleSignIn
-   */
-  async handleSignInCallback(callbackUrl: string) {
-    const nodeClient = await this.createNodeClient();
-
-    await nodeClient.handleSignInCallback(callbackUrl);
-  }
-
-  /**
-   * Get Logto context from cookies.
-   *
-   * @param config additional configs of GetContextParameters
-   * @returns LogtoContext
-   */
-  async getLogtoContext(config: GetContextParameters = {}) {
-    const nodeClient = await this.createNodeClient({ ignoreCookieChange: true });
-    const context = await nodeClient.getContext(config);
-
-    return context;
-  }
-
-  async createNodeClient({ ignoreCookieChange }: { ignoreCookieChange?: boolean } = {}) {
-    const { cookies } = await import('next/headers');
-    this.storage = new CookieStorage({
-      encryptionKey: this.config.cookieSecret,
-      cookieKey: `logto_${this.config.appId}`,
-      isSecure: this.config.cookieSecure,
-      getCookie: (...args) => {
-        return cookies().get(...args)?.value ?? '';
-      },
-      setCookie: (...args) => {
-        // In server component (RSC), it is not allowed to modify cookies, see https://nextjs.org/docs/app/api-reference/functions/cookies#cookiessetname-value-options.
-        if (!ignoreCookieChange) {
-          cookies().set(...args);
+    async handleSignIn(options, interactionMode) {
+        const nodeClient = await this.createNodeClient();
+        const finalOptions = typeof options === 'string' || options instanceof URL
+            ? { redirectUri: options, interactionMode }
+            : options;
+        await nodeClient.signIn(finalOptions);
+        if (!this.navigateUrl) {
+            // Not expected to happen
+            throw new Error('navigateUrl is not set');
         }
-      },
-    });
+        return {
+            url: this.navigateUrl,
+        };
+    }
+    /**
+     * Init sign-out and return the url to redirect to Logto.
+     *
+     * @param redirectUri the uri (postSignOutUri) to redirect to after sign out
+     * @returns the url to redirect to
+     */
+    async handleSignOut(redirectUri = this.config.baseUrl) {
+        const nodeClient = await this.createNodeClient();
+        await nodeClient.signOut(redirectUri);
+        await this.storage?.destroy();
+        if (!this.navigateUrl) {
+            // Not expected to happen
+            throw new Error('navigateUrl is not set');
+        }
+        return this.navigateUrl;
+    }
+    /**
+     * Handle sign-in callback from Logto.
+     *
+     * @param callbackUrl the uri (callbackUri) to redirect to after sign in, should match the one used in handleSignIn
+     */
+    async handleSignInCallback(callbackUrl) {
+        const nodeClient = await this.createNodeClient();
+        await nodeClient.handleSignInCallback(callbackUrl);
+    }
+    /**
+     * Get Logto context from cookies.
+     *
+     * @param config additional configs of GetContextParameters
+     * @returns LogtoContext
+     */
+    async getLogtoContext(config = {}) {
+        const nodeClient = await this.createNodeClient({ ignoreCookieChange: true });
+        const context = await nodeClient.getContext(config);
+        return context;
+    }
+    
+    async createNodeClient({ ignoreCookieChange } = {}) {
+        const { cookies } = await import('next/headers');
+        const cookiesData = await cookies();
+        this.storage = new CookieStorage({
+            encryptionKey: this.config.cookieSecret,
+            cookieKey: `logto:${this.config.appId}`,
+            isSecure: this.config.cookieSecure,
+            getCookie: (...args) => {
+                return cookiesData.get(...args)?.value ?? '';
+            },
+            setCookie: (...args) => {
+                // In server component (RSC), it is not allowed to modify cookies, see https://nextjs.org/docs/app/api-reference/functions/cookies#cookiessetname-value-options.
+                if (!ignoreCookieChange) {
+                    cookiesData.set(...args);
+                }
+            },
+        });
+        await this.storage.init();
+        return new this.adapters.NodeClient(this.config, {
+            storage: this.storage,
+            navigate: (url) => {
+                this.navigateUrl = url;
+            },
+        });
+    }
 
-    await this.storage.init();
-
-    return new this.adapters.NodeClient(this.config, {
-      storage: this.storage,
-      navigate: (url) => {
-        this.navigateUrl = url;
-      },
-    });
-  }
 }
+
+export { LogtoClient as default };
